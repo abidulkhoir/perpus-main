6ac6c535c30932013d835f1cde784f6f8ead5e05
abidulkhoir <abidulkhoir6@gmail.com>
2025-08-08 03:33:15 +0700

    Delete C2C023173E directory

--- C2C023173E/biodata/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include '../config/uts_web.php';
-$id = $_GET['id'];
-$conn->query("DELETE FROM biodata WHERE id=$id");
-header("Location: daftar.php");
-?>
--- C2C023173E/config/uts_web.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "root", "uts_web");
-if ($conn->connect_error) die("Koneksi gagal: " . $conn->connect_error);
-?>
